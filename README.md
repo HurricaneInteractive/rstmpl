e8fb56b075f5675a47b00e68f82648f6a78a789c
HurricaneInteractive <hurricane.interactive@gmail.com>
2022-01-12 20:19:14 +1100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Early access
+
+This README.md has been screenshotted and is being sold for $1mil as an NFT.
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use clap::{arg, App, AppSettings};
+
+fn main() {
+    let matches = App::new("rstmpl")
+        .about("Generate your own collection of templates with ease")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            App::new("clone")
+                .about("Clones a template")
+                .arg(arg!(<TEMPLATE> "The template name"))
+                .setting(AppSettings::ArgRequiredElseHelp),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("clone", sub_matches)) => {
+            println!(
+                "Cloning {}",
+                sub_matches.value_of("TEMPLATE").expect("required")
+            )
+        }
+        _ => unreachable!()
+    }
+}
