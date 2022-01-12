a2dc727eca5b3b6be99646829ad67dd292edf32b
HurricaneInteractive <hurricane.interactive@gmail.com>
2022-01-13 09:13:06 +1100

    feat: introduce dirs library

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use clap::{arg, App, AppSettings};
+use dirs;
 
 fn main() {
-    let matches = App::new("rstmpl")
-        .about("Generate your own collection of templates with ease")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(
-            App::new("clone")
-                .about("Clones a template")
-                .arg(arg!(<TEMPLATE> "The template name"))
-                .setting(AppSettings::ArgRequiredElseHelp),
-        )
-        .get_matches();
+  let matches = App::new("rstmpl")
+    .about("Generate your own collection of templates with ease")
+    .setting(AppSettings::SubcommandRequiredElseHelp)
+    .subcommand(
+      App::new("clone")
+        .about("Clones a template")
+        .arg(arg!(<TEMPLATE> "The template name"))
+        .setting(AppSettings::ArgRequiredElseHelp),
+    )
+    .get_matches();
 
-    match matches.subcommand() {
-        Some(("clone", sub_matches)) => {
-            println!(
-                "Cloning {}",
-                sub_matches.value_of("TEMPLATE").expect("required")
-            )
-        }
-        _ => unreachable!()
+  match matches.subcommand() {
+    Some(("clone", sub_matches)) => {
+      println!(
+        "Cloning {}",
+        sub_matches.value_of("TEMPLATE").expect("required")
+      );
+      println!("Home dir {:#?}", dirs::home_dir().unwrap());
     }
+    _ => unreachable!()
+  }
 }
