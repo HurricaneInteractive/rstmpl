a9462258eac129563bdfd099de89b9f0af6e94bc
HurricaneInteractive <hurricane.interactive@gmail.com>
2022-01-15 13:32:03 +1100

    feat: get files recursively

--- src/clone.rs
+++ src/clone.rs
@@ -1,10 +1,34 @@
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use std::{fs, io};
+use std::fs::{DirEntry};
 use std::io::{ErrorKind, Error};
 
 pub struct Clone {}
 
 impl Clone {
+  // @see https://doc.rust-lang.org/std/fs/fn.read_dir.html
+  fn visit_directories(dir: &Path) -> io::Result<Vec<PathBuf>> {
+    let mut entries: Vec<PathBuf> = Vec::new();
+
+    if dir.is_dir() {
+      for entry in fs::read_dir(dir)? {
+        let entry: DirEntry = entry?;
+        let path: PathBuf = entry.path();
+        if path.is_dir() {
+          let mut recursive_entries = Clone::visit_directories(&path)?;
+          entries.append(&mut recursive_entries);
+        } else {
+          // TODO: Create a global list of invalid filenames
+          if entry.file_name() != ".DS_Store" {
+            entries.push(path);
+          }
+        }
+      }
+    }
+
+    Ok(entries)
+  }
+
   pub fn execute(path: PathBuf) -> io::Result<()> {
     if !path.exists() {
       return Err(Error::new(ErrorKind::NotFound, "The template could not be found."))
@@ -12,10 +36,7 @@ impl Clone {
 
     println!("Cloning {:#?}", path);
 
-    // @see https://doc.rust-lang.org/std/fs/fn.read_dir.html
-    let entries: Vec<PathBuf> = fs::read_dir(path)?
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
+    let entries = Clone::visit_directories(&path)?;
 
     if entries.len() == 0 {
       return Err(Error::new(ErrorKind::Other, "Your template is empty"))
